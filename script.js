12c633b4cb9791d6bb87e80c8c6b513747bbfbff
Alex <alex.scheglov25@gmail.com>
2019-11-18 13:57:57 +0100

    This is my little javascript project

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 'use strict';
 
 
+var money = parseInt(prompt("How much money do you have for this mouth?"));
+var date = prompt("Write down the date in YYYY-MM-DD format");
 
-var leftBorderWidth = 1;
-let second = 2;
-const pi = 3.14;
+var q1 = prompt("Write your required expense this mouth:");
+var q2 = parseInt(prompt("How much will it cost?"));
 
-console.log(pi);
 
-var person = {
-    name: "John",
-    age: '18',
-    isMarried: false,
+var appData = {
+    money_ : money,
+    date_ : date,
+    income : [0,0,0,0],
+    optionalExpenses : 0,
+    savings : false,
+    expenses : {q1,q2},
 
 };
 
-console.log(person['age']);
+console.log(appData.expenses);
+console.log(appData.date_);
 
-let arr = ['plum.png','orange.jpg', 'apple.bmp'];
+var moneyDay = (money - q2) / 30;
+alert("You can spend " + parseInt(moneyDay) +" euros at day");
 
-console.log(arr[1]);
 
-//alert("Hello World");
-let answer = prompt("Tienes 18???", "SI");
-console.log(typeof(arr));
