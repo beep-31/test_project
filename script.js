cca28885295c93c1a980f0e8142ff66b2bce7a6e
Alex <alex.scheglov25@gmail.com>
2019-11-18 13:01:17 +0100

    This is my first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log(pi);
+
+var person = {
+    name: "John",
+    age: '18',
+    isMarried: false,
+
+};
+
+console.log(person['age']);
+
+let arr = ['plum.png','orange.jpg', 'apple.bmp'];
+
+console.log(arr[1]);
+
+//alert("Hello World");
+let answer = prompt("Tienes 18???", "SI");
+console.log(typeof(arr));
