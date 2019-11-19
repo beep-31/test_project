e3158ad24816dab044771319cc2a7c80992bf9bc
Alex <alex.scheglov25@gmail.com>
2019-11-19 14:39:46 +0100

    Some bugs were fixed in script.js

--- script.js
+++ script.js
@@ -4,9 +4,6 @@
 var money = parseInt(prompt("How much money do you have for this mouth?"));
 var date = prompt("Write down the date in YYYY-MM-DD format");
 
-var q1 = prompt("Write your required expense this mouth:");
-var q2 = parseInt(prompt("How much will it cost?"));
-
 
 var appData = {
     money_ : money,
@@ -18,21 +15,20 @@ var appData = {
 
 };
 
-for (let i = 0; i<2;i++){
-    var q1 = prompt("Write your required expense this mouth:"),
+for (let i = 0; i<2;++i){
+    var q1 = prompt("Describe your required expense this mouth: p.s it's not necessary"),
         q2 = parseInt(prompt("How much will it cost?"));
-    if ((typeof(q1))==='string' && (typeof(q1) != null) && (typeof(q2)) != null && q1!='' && q2 !=''){
-        appData.expenses[q1] = q2;
-    } else {
-        
-    }   
+        money = money - q2;
+    appData.expenses[q1] = q2;
    
 }
 
 console.log(appData.expenses);
 
-var moneyPerDay = (money - q2) / 30;
+var moneyPerDay = money / 30;
 appData.moneyPerDay = moneyPerDay;
 alert("You can spend " + parseInt(moneyPerDay) +" euros at day");
+alert("Try one more time!");
+
 
 
