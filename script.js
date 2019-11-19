7f9c989f8f99f0b3115a3c79004dced6702faaee
Alex <alex.scheglov25@gmail.com>
2019-11-19 13:36:24 +0100

    script.js was changed

--- script.js
+++ script.js
@@ -11,17 +11,28 @@ var q2 = parseInt(prompt("How much will it cost?"));
 var appData = {
     money_ : money,
     date_ : date,
-    income : [0,0,0,0],
-    optionalExpenses : 0,
+    income : [],
+    optionalExpenses : {},
     savings : false,
-    expenses : {q1,q2},
+    expenses : {},
 
 };
 
-console.log(appData.expenses);
-console.log(appData.date_);
+for (let i = 0; i<2;i++){
+    var q1 = prompt("Write your required expense this mouth:"),
+        q2 = parseInt(prompt("How much will it cost?"));
+    if ((typeof(q1))==='string' && (typeof(q1) != null) && (typeof(q2)) != null && q1!='' && q2 !=''){
+        appData.expenses[q1] = q2;
+    } else {
+        
+    }   
+   
+}
 
-var moneyDay = (money - q2) / 30;
-alert("You can spend " + parseInt(moneyDay) +" euros at day");
+console.log(appData.expenses);
+
+var moneyPerDay = (money - q2) / 30;
+appData.moneyPerDay = moneyPerDay;
+alert("You can spend " + parseInt(moneyPerDay) +" euros at day");
 
 
