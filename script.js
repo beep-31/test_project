e6200f83bd67a0c8c2a236b99c5171bf26235b25
Alex <alex.scheglov25@gmail.com>
2019-11-24 19:34:28 +0100

    optional expenses were added and getNumber func was changed

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function getNumber(num){
     if(num<0 || !parseInt(num)){
         while(isCorrect==false){
             num = parseInt(prompt("Write down a number"));
-            if(typeof(num)!='undefined' && num>0){
+            if(typeof(num)!='undefined' && num>=0){
                 isCorrect = true;
                 return num;
             } else{
@@ -22,7 +22,7 @@ function getNumber(num){
 let money;
 alert("Write down the amount of money you have for this mouth");
 money = getNumber(money);
-//money = checkNumber(money);
+
 console.log(money);
 console.log(typeof(money));
 
@@ -36,10 +36,13 @@ var appData = {
     savings : false,
     expenses : {},
     moneyPerDay_ : moneyPerDay,
+    optioanlMoneyDay_ : optioanlMoneyDay,
 
 };
 
-for (let i = 0; i<2;++i){
+let requieredExpenses = 0;
+//requiered expenses
+for (let i = 1; i<3;++i){
     let q1 = prompt("Describe your required expense this mouth: p.s it's not necessary");
     alert("How much will it cost?");
     let q2;
@@ -47,21 +50,49 @@ for (let i = 0; i<2;++i){
     console.log("required expense " + i +" is iqual to - " + q2);
     console.log(typeof(q2));
     appData.expenses[q1] = q2;
-    money -= q2;
+    requieredExpenses += q2;
    
 }
 
+let moneyOpt = money;
+let optionalTotal = 0;
+//optional expenses
+function optional(){
+    
+    for (let i=1; i < 4; i++){
+        let a1 = prompt("Any optional expenses? p.s it's not necessary(print 'none' if you don't have one)");
+        if(a1 == 'none'){
+            break;
+        }
+        alert('How much will it cost?');
+        let a2;
+        a2 = getNumber(a2);
+        optionalTotal+=a2;
+        appData.optionalExpenses[a1] = a2;
+        }
+}
+optional();
+
+//only requiered expenses
 if (money <= 0){
     var moneyPerDay = 0;
     appData.moneyPerDay_ = moneyPerDay;
     alert("Sorry, you have nothing to spend");
 
 } else { 
-
-    var moneyPerDay = money / 30;
-    appData.moneyPerDay = moneyPerDay;
+    var moneyPerDay = (money - requieredExpenses) / 30;
+    appData.moneyPerDay_ = moneyPerDay;
 
     alert("You can spend " + parseInt(moneyPerDay) + " euros per day");
+}
 
+//optional too
 
+if(optionalTotal==0){
+    alert("You don't have any optioanl expenses");
+} else{
+    var optioanlMoneyDay = 0; 
+    optioanlMoneyDay = (moneyOpt - optionalTotal - requieredExpenses) / 30;
+    alert("You can spend " + parseInt(optioanlMoneyDay) + " euros per day(Your optional expenses)");
+    appData.optioanlMoneyDay_ = optioanlMoneyDay;
 }
